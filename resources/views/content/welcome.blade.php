a44b58f2d383cdb65e175d90acb9ead107286d75
Renacuajo <braurrego@protonmail.com>
2023-01-15 13:28:06 -0500

    Pagina principal actualizada

--- resources/views/content/welcome.blade.php
+++ resources/views/content/welcome.blade.php
@@ -6,10 +6,14 @@
                 <h6 class="card-subtitle mb-2 text-muted text-center fs-5">Hecho por B.C.</h6>
                 <br>
                 <p class="card-text fs-5">
-                    Buenas, este es mi solución del desafío de desarrollo.
+                    ¡Buenas! Esta es mi solución del desafío de desarrollo.
                     Proyecto realizado en Laravel utilizando MySQL como base de datos,
                     Bootstrap para los estilos y componentes, y FullCalendar para mostrar el calendario.
                     <br> 
+                    
+                    <br> 
+                    La solución consta de dos páginas, una para las reservas y otra para mostrar la información
+                    en un calendario.
                 </p>
                 <a href="https://github.com/Zor3n/desafio-ce" class="card-link fs-5" target="_blank">Código en GitHub</a>
             </div>
