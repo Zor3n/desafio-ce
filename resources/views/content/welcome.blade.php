3159ca85f6117e671a3f723a13a6388883e67c8a
Renacuajo <braurrego@protonmail.com>
2023-01-14 22:37:10 -0500

    Pagina de bienvenida

--- resources/views/content/welcome.blade.php
+++ resources/views/content/welcome.blade.php
@@ -1,5 +1,19 @@
 <x-app-layout>
     <div class="container pt-5 pb-5">
-        Hello World!
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title text-center fs-1">Desafío de desarrollo - CDE</h5>
+                <h6 class="card-subtitle mb-2 text-muted text-center fs-5">Hecho por B.C.</h6>
+                <br>
+                <p class="card-text fs-5">
+                    Buenas, este es mi solución del desafío de desarrollo.
+                    Proyecto realizado en Laravel utilizando MySQL como base de datos y
+                    Bootstrap para los estilos y componentes.
+                    <br>
+                    He hecho una página por cada uno de los requerimientos del desafío. 
+                </p>
+                <a href="https://github.com/Zor3n/desafio-ce" class="card-link fs-5" target="_blank">Código en GitHub</a>
+            </div>
+        </div>
     </div>
 </x-app-layout>
